2d13c2aa43699e27bdcbfff23379c1c4e5a95398
Florian Krüger <florian.krueger@projectserver.org>
2016-08-29 17:26:22 +0200

    Reintegrate SampleApp to unmark dependency schemes as shared (#32)

---
+++ SampleApp/Pods/Target Support Files/Pods-ConsistencyManagerDemo/Pods-ConsistencyManagerDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ConsistencyManagerDemo : NSObject
+@end
+@implementation PodsDummy_Pods_ConsistencyManagerDemo
+@end
---
+++ SampleApp/Pods/Target Support Files/Pods-ConsistencyManagerDemo/Pods-ConsistencyManagerDemo-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ConsistencyManagerDemoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ConsistencyManagerDemoVersionString[];
+
