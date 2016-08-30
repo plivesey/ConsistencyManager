807b3217935fa6b6648acf5318fb878ff1de04d8
Peter Livesey <plivesey453@gmail.com>
2016-08-30 09:37:53 -0700

    Adding support for OSX (#33)

--- ConsistencyManager/ConsistencyManager.h
+++ ConsistencyManager/ConsistencyManager.h
@@ -7,7 +7,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for ConsistencyManager.
 FOUNDATION_EXPORT double ConsistencyManagerVersionNumber;
--- build.sh
+++ build.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
+# Builds all targets and runs tests.
+
+DERIVED_DATA=${1:-/tmp/ConsistencyManager}
+echo "Derived data location: $DERIVED_DATA";
 
 set -o pipefail &&
+rm -rf $DERIVED_DATA &&
 time xcodebuild clean test \
     -project ConsistencyManager.xcodeproj \
     -scheme ConsistencyManager \
+    -sdk macosx10.11 \
+    -derivedDataPath $DERIVED_DATA \
+    | tee build.log \
+    | xcpretty &&
+rm -rf $DERIVED_DATA &&
+time xcodebuild clean build \
+    -project ConsistencyManager.xcodeproj \
+    -scheme ConsistencyManager \
     -sdk iphonesimulator9.3 \
-    -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' \
+    -derivedDataPath $DERIVED_DATA \
     -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.4' \
-| xcpretty
+    -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' \
+    | tee build.log \
+    | xcpretty &&
+cat build.log
 
