5365967c0bff89ddb0c0f01fe524ef7366845d28
Peter Livesey <plivesey@linkedin.com>
2016-09-19 07:41:30 -0700

    Reenabling iOS 8+9 simulators

--- build.sh
+++ build.sh
@@ -19,9 +19,8 @@ time xcodebuild clean build \
     -scheme ConsistencyManager \
     -sdk iphonesimulator10.0 \
     -derivedDataPath $DERIVED_DATA \
-# Disabling these until travis adds them
-#    -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.4' \
-#    -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' \
+    -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.4' \
+    -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' \
     -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.0' \
     | tee build.log \
     | xcpretty &&
